0319096890fb11952904ed763f06c25a1b4a4426
Lucas Oktay <lucasoktay2025@u.northwestern.edu>
2024-10-16 19:48:00 -0500

    black keys fixed

--- app/piano/piano.js
+++ app/piano/piano.js
@@ -1,69 +1,78 @@
 import { useRef, useState } from 'react';
 import { ScrollView, View } from "react-native";
+import { GestureHandlerRootView, PanGestureHandler, State, TapGestureHandler } from 'react-native-gesture-handler';
 import NavBar from "../navbar/navbar";
 import { Piano } from '../react-native-piano/index.js';
-// import { loadSounds, loadedSounds } from '../sounds/sounds.js';
 import styles from "../styles.js";
 import NoteLabel from './notelabel.js'; // Import the NoteLabel component
 
 const PianoComponent = ({ handlePlaySound }) => {
     const [pianoData, setPianoData] = useState([]);
-    const [isScrolling, setIsScrolling] = useState(false);
     const [currentNote, setCurrentNote] = useState(null);
-    const [sound, setSound] = useState();
     const scrollViewRef = useRef(null);
+    const panRef = useRef(null);
+    const tapRef = useRef(null);
 
     const handlePlayNote = async (note) => {
         setPianoData(pianoData => [...pianoData, note]);
         handlePlaySound(note);
     };
 
+    handleTouchStart = (midiNumber) => {
+        console.log('Touch start');
+        setCurrentNote(midiNumber);
+    }
+
     const handleStopNote = () => {
         // console.log(pianoData);
     };
 
-    const handleTouchStart = (midiNumber) => {
-        setIsScrolling(false);
-        setCurrentNote(midiNumber);
-    };
-
-    const handleTouchMove = () => {
-        console.log('touch move');
-        setIsScrolling(true);
-    };
-
-    const handleTouchEnd = async () => {
-        console.log("touch end, scrolling: ", isScrolling);
-        if (!isScrolling && currentNote !== null) {
-            await handlePlaySound(currentNote);
+    const handleTapGestureEvent = async (event) => {
+        if (event.nativeEvent.state === State.END) {
+            if (currentNote !== null) {
+                await handlePlaySound(currentNote);
+            }
         }
-        setCurrentNote(null);
     };
 
     return (
-        <View style={{ backgroundColor: '#F9F5F1', flexGrow: 1 }}>
-            <View style={styles.fullscreenpiano}>
-                <ScrollView
-                    horizontal={true}
-                    style={styles.pianoscrollview}
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                    contentOffset={{ x: 700, y: 0 }} // Set the initial scroll position
-                    onTouchMove={handleTouchMove}
-                >
-                    <View style={styles.pianowrapper}>
-                        <Piano
-                            noteRange={{ first: 'c1', last: 'c7' }}
-                            style={styles.piano}
-                            onPlayNoteInput={midiNumber => handleTouchStart(midiNumber)}
-                            onStopNoteInput={midiNumber => handleTouchEnd()}
-                            children={midiNumber => <NoteLabel midiNumber={midiNumber} />}
-                        />
-                    </View>
-                </ScrollView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <View style={{ backgroundColor: '#F9F5F1', flexGrow: 1 }}>
+                <View style={styles.fullscreenpiano}>
+                    <PanGestureHandler
+                        onGestureEvent={() => { }}
+                        onHandlerStateChange={() => { }}
+                        waitFor={tapRef}
+                        ref={panRef}
+                    >
+                        <ScrollView
+                            horizontal={true}
+                            style={styles.pianoscrollview}
+                            showsVerticalScrollIndicator={false}
+                            showsHorizontalScrollIndicator={false}
+                            contentOffset={{ x: 700, y: 0 }} // Set the initial scroll position
+                            ref={scrollViewRef}
+                        >
+                            <TapGestureHandler
+                                onHandlerStateChange={handleTapGestureEvent}
+                                ref={tapRef}
+                            >
+                                <View style={styles.pianowrapper}>
+                                    <Piano
+                                        noteRange={{ first: 'c1', last: 'c7' }}
+                                        style={styles.piano}
+                                        onPlayNoteInput={midiNumber => handleTouchStart(midiNumber)}
+                                        onStopNoteInput={midiNumber => handleStopNote()}
+                                        children={midiNumber => <NoteLabel midiNumber={midiNumber} />}
+                                    />
+                                </View>
+                            </TapGestureHandler>
+                        </ScrollView>
+                    </PanGestureHandler>
+                </View>
+                <NavBar />
             </View>
-            <NavBar />
-        </View>
+        </GestureHandlerRootView>
     );
 };
 
